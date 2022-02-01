52882c0f98fa36313f063cab316218d7bd11a3b1
Piotr Olejarz <piotr.olejarz@peak11.com>
2022-02-01 23:25:59 +0100

    editing and adding context

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,20 +1,19 @@
-import React,{useContext} from 'react';
+import React, {useContext} from 'react';
 import {GlobalContext} from '../context/GlobalState';
-const  TransactionList = () => {
-    const context = useContext(GlobalContext);
-    
+import Transaction from './Transaction'
+
+const TransactionList = () => {
+    const {transactions} = useContext(GlobalContext);
+
     return (
         <div>
             <h3>History</h3>
             <ul id='list' className='list'>
-        {transactions.map(transaction=>(  <li className='minus'>
-        {transaction.text}<span>$400</span>
-                    <button className='delete-btn'>X</button>
-                </li>  ))}
-               
+                {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}
+
             </ul>
         </div>
     );
 };
 
-export default  TransactionList;
+export default TransactionList;
