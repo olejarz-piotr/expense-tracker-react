a327ebfcc60c7ca943794320251f281146e2a9c3
Piotr Olejarz <piotr.olejarz@peak11.com>
2022-02-02 00:03:00 +0100

    ending commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,10 +4,7 @@ import AppReducer from './AppReducer'
 //initial state
 const initialState={
     transactions:[
-        {id:1, text:'Flower', amount:-20},
-        {id:2, text:'Pen', amount:300},
-        {id:3, text:'Magazine', amount:10},
-        {id:4, text:'Bike', amount:2},
+
 
     ]
 
@@ -20,7 +17,22 @@ export const GlobalContext= createContext(initialState)
 
 export const GlobalProvider=({children})=>{
     const [state,dispatch]=useReducer(AppReducer,initialState);
-    return (<GlobalContext.Provider value={{transactions:state.transactions}}>
+
+    function deleteTransaction(id){
+        dispatch({
+            type:'DELETE_TRANSACTION',
+            payload:id
+        })
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type:"ADD_TRANSACTION",
+            payload:transaction
+        })
+    }
+
+    return (<GlobalContext.Provider value={{transactions:state.transactions,deleteTransaction,addTransaction}}>
         {children}
     </GlobalContext.Provider>)
 }
