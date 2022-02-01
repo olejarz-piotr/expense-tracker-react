85502905829878ac0eeed0ea54a5fe057289d874
Piotr Olejarz <olejarz-piotr@o2.pl>
2022-02-01 12:20:21 +0100

    Update TransactionList.js
    Using  GlobalContext to show transactionList

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,17 @@
-import React from 'react';
-
+import React,{useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
 const  TransactionList = () => {
+    const context = useContext(GlobalContext);
+    
     return (
         <div>
             <h3>History</h3>
             <ul id='list' className='list'>
-                <li className='minus'>
-                    Cash<span>$400</span>
+        {transactions.map(transaction=>(  <li className='minus'>
+        {transaction.text}<span>$400</span>
                     <button className='delete-btn'>X</button>
-                </li>
+                </li>  ))}
+               
             </ul>
         </div>
     );
