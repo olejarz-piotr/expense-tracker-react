6b430fd74ea8c6b7c3edcf980a0f432fd436e02b
Piotr Olejarz <olejarz-piotr@o2.pl>
2022-02-01 12:16:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
 import './App.css';
 import React  from "react";
+import {GlobalProvider} from './context/GlobalState';
 function App() {
   return (
-    <div >
+    <GlobalProvider>
     <Header/>
       <div className="container">
         <Balance/>
@@ -15,7 +16,7 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
